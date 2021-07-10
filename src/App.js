6be96e520c38cc5afa61c4ad8a9b74c8c98d8775
Dinesh Bhagwat <drbhagwat@yahoo.com>
2021-07-10 22:03:57 +0530

    Finally worked

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ function App() {
   return (
     <Layout>
       <Switch>
-        <Route path="/" exact={true}>
+        <Route path="/" exact>
           <AllMeetupsPage />
         </Route>
         <Route path="/new-meetup">
